331ad375d91c54a63c414acab58c4f59566d8a0c
merveltin <154346159+merveltin@users.noreply.github.com>
2024-01-04 17:11:15 +0300

    Add files via upload

---
+++ NewWorld.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewWorld : MonoBehaviour
+{
+  void Bolenleribul(int ilksayi, int ikincisayi);
+   
+
+    
+
+
+   private void Start()
+    {
+        Bolenleribul(6, 77);
+
+    }
+        
+    }
+   void Update()
+    {
+    print("hello");
+
+    }
+}
